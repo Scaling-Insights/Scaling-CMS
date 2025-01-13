33df5b5b0a30c827f34daa893ecd656cf7d11dc3
Jasper <Jaspervanwilligen03@gmail.com>
2025-01-13 13:52:01 +0100

    2 cypress tests 🧪

--- frontend/cypress/e2e/UC3.spec.ts
+++ frontend/cypress/e2e/UC3.spec.ts
@@ -5,6 +5,13 @@ describe('Login Page', () => {
       cy.visit('/'); 
   });
 
+  Cypress.on('uncaught:exception', (err) => {
+    if (err.message.includes('An error occurred while posting data')) {
+      return false; 
+    }
+    return true; 
+  });
+
   it('should display validation error when only email is filled in', () => {
     cy.fixture('mocks/login.json').then((data) => {
         cy.get('input[name="email"]').type(data.email);
@@ -61,4 +68,41 @@ describe('Login Page', () => {
       cy.url().should('include', '/channel');
     });
   });
+
+  it('should redirect to login page when accessing /channel while not logged in', () => {
+   
+    cy.visit('/channel', { failOnStatusCode: false });
+
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
+
+    cy.get('form').should('be.visible');
+    cy.get('input[name="email"]').should('exist');
+    cy.get('input[name="password"]').should('exist');
+  });
+
+  it('should redirect to /channel when accessing the base URL while logged in', () => {
+    cy.intercept('POST', '/auth/login', {
+      fixture: 'mocks/authLogin.json',
+    }).as('loginRequest');
+
+    cy.intercept('POST', '/content/get-range', {
+      statusCode: 401, 
+      body: {}, 
+    }).as('getRangeRequest');
+  
+    cy.visit('/');
+    cy.fixture('mocks/login.json').then((data) => {
+      cy.get('input[name="email"]').type(data.email);
+      cy.get('input[name="password"]').type(data.password);
+      cy.get('#loginButton').click();
+  
+      cy.wait('@loginRequest');
+  
+      cy.url().should('include', '/channel');
+  
+      cy.visit('/');
+  
+      cy.url().should('include', '/channel');
+    });
+  });  
 });
